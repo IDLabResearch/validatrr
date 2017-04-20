b476e56dea3a8384e82d0d03652775fa8f80d206
Ben De Meester <ben.demeester@ugent.be>
2017-04-20 17:08:36 +0200

    cmd

--- validate.js
+++ validate.js
@@ -1,9 +1,8 @@
-const spawn = require('child_process').spawn;
 const path = require('path');
 const os = require("os");
 const fs = require("fs");
-const N3 = require('n3');
 const Validator = require('./lib/Validator');
+const argv = require('minimist')(process.argv.slice(2));
 
 let profile = 'owl';
 let distPath = path.resolve(__dirname, 'dist', 'n3unit-owl.pvm');
@@ -15,17 +14,25 @@ let validator = new Validator({
   extraFiles: [distPath, profilePath]
 });
 
-let sourcePath = path.resolve(__dirname, '..', 'dataset-downloader', 'output/1000/foaf/897fe17c9c53b2890eeb0328d5d0ad63/897fe17c9c53b2890eeb0328d5d0ad63');
+let sourcePath = argv.i;
+let outputPath = argv.o;
+let schemas = argv.s ? argv.s.split(',') : null;
+
+if (schemas) {
+  const prefices = JSON.parse(fs.readFileSync(path.resolve('resources', 'ontologies', 'prefix.json')));
+  schemas = schemas.map(function (schema) {
+    return prefices[schema] ? prefices[schema] : schema
+  });
+}
 
 let sourceData = fs.readFileSync(sourcePath, 'utf8');
 
 const startTime = new Date();
-validator.validate(sourceData, ['http://purl.org/dc/terms/'], function (err, stdout, stderr) {
+validator.validate(sourceData, schemas, function (err, stdout, stderr) {
   const endTime = new Date();
   if (err) {
     throw err;
   }
-  console.log(stdout);
-  console.error(stderr);
+  fs.writeFileSync(outputPath, stdout, 'utf8');
   console.log(`done in ${endTime.getTime() - startTime.getTime()}!`);
 });
