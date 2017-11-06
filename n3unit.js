15bd6b1f685cb68e583efdf3071c6cb84e885e30
Ben De Meester <ben.demeester@ugent.be>
2017-11-06 15:20:40 +0100

    added all node tests

--- n3unit.js
+++ n3unit.js
@@ -2,9 +2,38 @@ const path = require('path');
 const EyeHandler = require('./lib/EyeHandler');
 
 let data = [
-  // 'tests/shacl/core/node/class-001.ttl',
-  // 'tests/shacl/core/node/class-002.ttl',
-  'tests/shacl/core/node/class-003.ttl',
+//   'tests/shacl/core/node/and-001.ttl', // TODO
+//   'tests/shacl/core/node/and-002.ttl', // TODO
+//   'tests/shacl/core/node/class-001.ttl',
+//   'tests/shacl/core/node/class-002.ttl',
+//   'tests/shacl/core/node/class-003.ttl',
+//   'tests/shacl/core/node/closed-001.ttl', // TODO
+//   'tests/shacl/core/node/closed-002.ttl', // TODO
+//   'tests/shacl/core/node/datatype-001.ttl', // TODO
+//   'tests/shacl/core/node/datatype-002.ttl', // TODO
+//   'tests/shacl/core/node/disjoint-001.ttl', // TODO
+//   'tests/shacl/core/node/equals-001.ttl', // TODO
+//   'tests/shacl/core/node/hasValue-001.ttl', // TODO
+//   'tests/shacl/core/node/in-001.ttl', // TODO
+//   'tests/shacl/core/node/languageIn-001.ttl', // TODO
+//   'tests/shacl/core/node/maxExclusive-001.ttl', // TODO
+//   'tests/shacl/core/node/maxInclusive-001.ttl', // TODO
+//   'tests/shacl/core/node/maxLength-001.ttl', // TODO
+//   'tests/shacl/core/node/minExclusive-001.ttl', // TODO
+//   'tests/shacl/core/node/minInclusive-001.ttl', // TODO
+//   'tests/shacl/core/node/minInclusive-002.ttl', // TODO
+//   'tests/shacl/core/node/minInclusive-003.ttl', // TODO
+//   'tests/shacl/core/node/minLength-001.ttl', // TODO
+//   'tests/shacl/core/node/node-001.ttl', // TODO
+//   'tests/shacl/core/node/nodeKind-001.ttl', // TODO
+//   'tests/shacl/core/node/not-001.ttl', // TODO
+//   'tests/shacl/core/node/not-002.ttl', // TODO
+//   'tests/shacl/core/node/or-001.ttl', // TODO
+//   'tests/shacl/core/node/pattern-001.ttl', // TODO
+//   'tests/shacl/core/node/pattern-002.ttl', // TODO
+//   'tests/shacl/core/node/xone-001.ttl', // TODO
+//   'tests/shacl/core/node/xone-duplicate.ttl', // TODO
+//   'tests/shacl/core/node/qualified-001.ttl', // TODO
 ];
 
 let baseRulesPath = path.resolve(__dirname, 'dist/n3unit.pvm');
